28f81d045e8c941ac02df16b82cbb0c180a82d5b
BHARGAVA ABHIRAM UMMADISHETTY <91135759+abhiram-ummadishetty@users.noreply.github.com>
2022-04-26 14:22:17 +0530

    Unit-Converter

---
+++ main.c
@@ -0,0 +1,67 @@
+#include <stdio.h>
+
+int main() {
+  //defining required variables
+  char category;
+  int tempChoice;
+  int massChoice;
+  int userinputF; 
+  int userinputC; 
+  int userinputGram; 
+  int fahrenheitToCelcius; 
+  int userinputOunce;
+  int celciusToFahrenheit; 
+  float ounceToPounds;
+  float gramsToPounds; 
+  
+  printf("Welcome to Unit Converter! \n");
+  printf("Here is a list of conversation to choose from: \n");
+  printf("Temperature(T),Mass(M) \n");
+  printf("Please enter the letter you want to convert.\n");
+  scanf("%c",&category);
+  
+  if(category == 'T'){
+      printf("Welcome to Temperature Converter! \n");
+      printf("Here is a list of conversations to choose from: \n");
+      printf("Enter 1 for Fahrenheit to Celsius. \n");
+      printf("Enter 2 for Celsius to Fahrenheit. \n");
+      scanf("%d",&tempChoice);
+      if(tempChoice == 1){
+          printf("Please enter the Fahrenheit degree: \n");
+          scanf("%d",&userinputF);
+          fahrenheitToCelcius =  ((userinputF-32) * (5.0/9.0));
+          printf("Celcius: %d",fahrenheitToCelcius);
+      }
+      else if(tempChoice == 2){
+        printf("Please enter the Celcius degree: \n");
+        scanf("%d",&userinputC);
+        celciusToFahrenheit = ((9.0/5.0)*userinputC + 32);
+        printf("Fahrenheit: %d",celciusToFahrenheit);
+      }
+      else 
+        printf("Please enter the valid choice. \n");
+  }
+ 
+  else if(category == 'M'){
+      printf("Welcome to Mass Converter! \n");
+      printf("Here is a list of conversations to choose from: \n");
+      printf("Enter 1 for ounces to pounds. \n");
+      printf("Enter 2 for gram to pounds. \n");
+      scanf("%d",&massChoice);
+      if(massChoice == 1){
+          printf("Please enter the ounce amount: \n");
+          scanf("%d",&userinputOunce);
+          ounceToPounds = userinputOunce * 0.0625;
+          printf("Pounds: %.2f",ounceToPounds);
+      }
+      else if(massChoice == 2) {
+          printf("Please enter the gram amount: \n");
+          scanf("%d",&userinputGram);
+          gramsToPounds = userinputGram * 0.00220462;
+          printf("Pounds: %.2f",gramsToPounds);
+      }
+      else 
+        printf("Please enter the correct choice. \n");
+   }
+  return 0;
+}
